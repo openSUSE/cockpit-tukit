68794719cee4894d1d3230eafd867f3e40c054bb
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2022-02-17 17:35:34 +0100

    Add missing headers

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2022 SUSE LLC
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
+ *
+ * To contact SUSE LLC about this file by physical or electronic mail, you may
+ * find current contact information at www.suse.com.
+ */
+
 export const stringToBool = (s) => {
     return ["yes", "true", "1"].includes(s.toLowerCase());
 };
