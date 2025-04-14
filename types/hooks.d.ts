8a9b48ec7165f64c0a0a8d5ceec15f3f6c1aae82
Luna D Dragon <luna.dragon@suse.com>
2025-04-14 18:39:51 +0530

    use typescript types provided by upstream

--- types/hooks.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Superuser } from "superuser";
-
-declare module "hooks" {
-	function useEvent(obj: Superuser, event: "changed"): void;
-}
--- types/superuser.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "superuser" {
-	type Superuser = {
-		allowed: boolean | null;
-		reload_page_on_change(): void;
-	};
-
-	const superuser: Superuser;
-}
